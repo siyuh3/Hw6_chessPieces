656ec709a3cca0b051e13aae049deb4893340fa3
Kicho Yu <k.yu@anchorfree.com>
2021-02-27 14:06:00 +0900

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,10 +1,26 @@
 /**
  * @program: Hw6_chessPieces
  * @description:
- * @author: Siyu Hou
+ * @author: Siyu Hou, Kicho Yu
  * @create: 2021-02-26 02:40
  **/
 public class Knight implements ChessPiece{
+    private int row;
+    private int column;
+    private Color color;
+
+    public Knight(int row, int column, Color color){
+        setRow(row);
+        setColumn(column);
+        setColor(color);
+    }
+
+    public Knight(Color color){
+        setRow(0);
+        setColumn(0);
+        setColor(color);
+    }
+    
     @Override
     public int getRow() {
         return 0;
@@ -19,7 +35,19 @@ public class Knight implements ChessPiece{
     public Color getColor() {
         return null;
     }
+    
+    public void setRow(int row) {
+        this.row = row;
+    }
 
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+    
     @Override
     public boolean canMove(int row, int col) {
         return false;
