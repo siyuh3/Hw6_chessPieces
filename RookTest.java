81d653a43611299763b101555616167c541e9388
siyuh3 <75604166+siyuh3@users.noreply.github.com>
2021-03-05 00:43:05 -0800

    add more test cases

--- RookTest.java
+++ RookTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.junit.Before;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 /**
  * A Junit test that tests the Rook class.
@@ -10,8 +11,9 @@ public class RookTest {
     private Rook rook2;
 
     @Before
-    public void setUp(){
-        rook1 = new Rook(0,0, Color.WHITE);
+    public void setUp() {
+        rook1 = new Rook(0, 0, Color.WHITE);
+        rook2 = new Rook(0, 7, Color.BLACK);
     }
 
     /**
@@ -19,54 +21,45 @@ public class RookTest {
      */
     @Test
     public void getRowTest() {
-        assertEquals(0, rook1.getCol());
+        assertEquals(0, rook1.getRow());
+        assertEquals(0, rook2.getRow());
     }
 
     /**
      * Tests the getCol method.
      */
     @Test
-    public void getColTest(){
+    public void getColTest() {
         assertEquals(0, rook1.getCol());
+        assertEquals(7, rook2.getCol());
     }
 
     /**
      * Tests the getColor method.
      */
     @Test
-    public void getColor(){
+    public void getColor() {
         assertEquals(Color.WHITE, rook1.getColor());
+        assertEquals(Color.BLACK, rook2.getColor());
     }
-
-//    @Test
-//    public void setRowTest(){
-//        assertEquals(2, pawn1.setRow(row));
-//    }
-
-//    @Test
-//    public void setColumnTest(){
-//
-//    }
-
-//    @Test
-//    public void setColorTest(){
-//
-//    }
 
     /**
      * Tests the canMove method.
      */
     @Test
-    public void canMoveTest(){
-        rook1.canMove(3,0);
+    public void canMoveTest() {
+        assertTrue(rook1.canMove(0, 1));
+        assertFalse(rook1.canMove(0, 0));
     }
 
     /**
      * Tests the canKill method.
      */
     @Test
-    public void canKill(){
+    public void canKill() {
         ChessPiece pawn1 = new Pawn(6, 0, Color.BLACK);
-        rook1.canKill(pawn1);
+        ChessPiece pawn2 = new Pawn(6, 1, Color.BLACK);
+        assertTrue(rook1.canKill(pawn1));
+        assertFalse(rook1.canKill(pawn2));
     }
 }
