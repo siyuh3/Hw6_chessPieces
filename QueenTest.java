7b3c481a9a40b2dc454838a993a5396b29417e49
siyuh3 <75604166+siyuh3@users.noreply.github.com>
2021-03-05 00:04:55 -0800

    tried more test cases

--- QueenTest.java
+++ QueenTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.junit.Before;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 /**
  * A JUnit test that tests the Queen class.
@@ -10,63 +11,56 @@ public class QueenTest {
     private Queen queen2;
 
     @Before
-    public void setUp(){
-        queen1 = new Queen(0,4, Color.WHITE);
+    public void setUp() {
+        queen1 = new Queen(0, 3, Color.WHITE);
+        queen2 = new Queen(7, 3, Color.BLACK);
     }
 
     /**
      * Tests the get row method.
      */
     @Test
-    public void getRowTest(){
+    public void getRowTest() {
         assertEquals(0, queen1.getRow());
+        assertEquals(7, queen2.getRow());
     }
 
     /**
      * Tests the getCol method.
      */
     @Test
-    public void getColTest(){
-        assertEquals(4, queen1.getCol());
+    public void getColTest() {
+        assertEquals(3, queen1.getCol());
+        assertEquals(3, queen2.getCol());
     }
 
     /**
      * Tests the getColor method.
      */
     @Test
-    public void getColor(){
+    public void getColor() {
         assertEquals(Color.WHITE, queen1.getColor());
+        assertEquals(Color.BLACK, queen2.getColor());
     }
-
-//    @Test
-//    public void setRowTest(){
-//        assertEquals(2, pawn1.setRow(row));
-//    }
-
-//    @Test
-//    public void setColumnTest(){
-//
-//    }
-
-//    @Test
-//    public void setColorTest(){
-//
-//    }
 
     /**
      * Tests the canMove method.
      */
     @Test
-    public void canMoveTest(){
-        queen1.canMove(1,5);
+    public void canMoveTest() {
+        assertTrue(queen1.canMove(7, 3));
+        assertTrue(queen1.canMove(3, 0));
+        assertFalse(queen2.canMove(0, 4));
     }
 
     /**
      * Tests the canKill method.
      */
     @Test
-    public void canKill(){
-        ChessPiece pawn2 = new Pawn(6, 4, Color.BLACK);
-        queen1.canKill(pawn2);
+    public void canKill() {
+        ChessPiece pawn2 = new Pawn(6, 4, Color.WHITE);
+        ChessPiece pawn3 = new Pawn(1, 0, Color.BLACK);
+        assertTrue(queen2.canKill(pawn2));
+        assertFalse(queen1.canKill(pawn3));
     }
 }
