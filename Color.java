57dbdd43a0cc2ad59389a47d2a6e57afec9019b1
Meiwang1248 <meiwang1248@hotmail.com>
2021-03-06 21:30:05 -0800

    Added description

--- Color.java
+++ Color.java
@@ -1,6 +1,6 @@
 /**
  * @program: Hw6_chessPieces
- * @description: a enumerate class for the colors of a chess piece
+ * @description: An enumerate class for the colors of a chess piece
  * @author: Siyu Hou
  * @create: 2021-02-26 02:47
  * Test
